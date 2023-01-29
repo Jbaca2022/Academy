837f748f0971a60a085a5cd2dffc37fa39d9e4f2
Cesar Baca <cesargenarobacasolis@gmail.com>
2023-01-29 13:27:47 -0500

    modified:   modelo/cnx.php

--- modelo/cnx.php
+++ modelo/cnx.php
@@ -3,10 +3,10 @@
 //Conexion Academia POSTGRESQL
 function conectaPgSQL() {
      $servidor = 'localhost';    
-    $port = 5432;
+    $port = 5433;
     $usuario = 'postgres';
     $clave = 'root';
-    $nombrebd = 'academia'; 
+    $nombrebd = 'academia';
     $str = 'host=' . $servidor . ' port=' . $port . ' dbname=' . $nombrebd . ' user=' . $usuario . ' password=' . $clave;
     $link = pg_connect($str) or die('No se ha podido conectar');
     return $link;
