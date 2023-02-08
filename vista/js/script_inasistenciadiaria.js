ce70ecb324f1e119482663a1b06167874878e98b
Cesar Baca <cesargenarobacasolis@gmail.com>
2023-02-07 22:29:30 -0500

    modified:   vista/asistencia/inasistenciadiaria.php 	modified:   vista/js/script_inasistenciadiaria.js

--- vista/js/script_inasistenciadiaria.js
+++ vista/js/script_inasistenciadiaria.js
@@ -7,6 +7,17 @@ $(document).ready(function () {
         }, function (data) {
             if (data != null) {
                 var html = '';
+
+                html += '<table id="tbl-inasistencia" cellspacing="0" width="100%">';
+                html += '<thead class="cabecera">';
+                html += '    <tr>';
+                html += '        <th>Apellido y Nombres</th>';
+                html += '        <th>Aula</th>';
+                html += '        <th>Carrera</th>';
+                html += '        <th>Teléfono</th>';
+                html += '    </tr>';
+                html += '</thead>';
+                html += '<tbody id="tbody-inasistencia">';
                 for (var i = 0; i < data.length; i++) {
                     html += '<tr class="tr-persona" id="'+ data[i].idpersona+'" data-nombre="'+ data[i].alumno +'"> '
                     html += '<td class="text-center">' + data[i].alumno + "</td>";
@@ -15,17 +26,36 @@ $(document).ready(function () {
                     html += '<td class="text-center">' + data[i].telefono + "</td>";
                     html += "</tr>";  
                 }
-                $('#tbody-inasistencia').html(html);
+                 html += '</tbody>';
+                 html += '</table>';
+                $('.tbl-asistencia').html(html);
                 $("#tbl-inasistencia").DataTable({
-                    searching: false,
                     ordering: false,
-                    bLengthChange: false
+                    bLengthChange: false,
+                    "searching": false,
+                    "language": {
+                        "url": "../json/Spanish.json"
+                    },
+                    "scrollCollapse": true,
+                    "paging": false                
                 });
             }
         }, 'json');
     }
     $('#txtfecha').change(function () {
         cargar_inasistencia();
+    });
+
+    $('#btnimprimir').click(function () {
+        var css = '';
+        css += '<link href="../../vista/css/bootstrap-print.css" rel="stylesheet" type="text/css" />';
+        var objeto = '';
+        objeto += '<h2><center><b>REPORTE DE INASISTENCIA</b></center></h2>'; 
+        objeto += '<h3><center>'+ $('#txtfecha').val() +'</center></h3>'; //obtenemos el objeto a imprimir
+        objeto += $('.divimprimir').html(); //obtenemos el objeto a imprimir
+        objeto += '<script>window.print();window.close();</script>';
+        var ventana = window.open('', '_blank'); //abrimos una ventana vacía nueva
+        ventana.document.write(css + objeto);
     });
 });
 
