93a368957dc0d7b394372acc885bdcd221309b51
Cesar Baca <cesargenarobacasolis@gmail.com>
2023-02-07 22:02:35 -0500

    modified:   vista/js/script_asistenciaindividual.js

--- vista/js/script_asistenciaindividual.js
+++ vista/js/script_asistenciaindividual.js
@@ -82,14 +82,14 @@ $(document).ready(function () {
                             (data[j]['dow'] == 5) ? Viernes = data[j]['diam']: '';
                             (data[j]['dow'] == 6) ? Sabado = data[j]['diam']: '';
                             (data[j]['dow'] == 0) ? Domingo = data[j]['diam']: '';
-                            hora = (data[j]['horam'] + data[j]['horasalida']);
+                            hora = (data[j]['horam'] + ' - ' + data[j]['horasalida']);
                             (data[j]['dow'] == 1) ? LunesHora_M = hora: '';
                             (data[j]['dow'] == 2) ? MartesHora_M = hora: '';
                             (data[j]['dow'] == 3) ? MiercolesHora_M = hora: '';
                             (data[j]['dow'] == 4) ? JuevesHora_M = hora: '';
                             (data[j]['dow'] == 5) ? ViernesHora_M = hora: '';
                             (data[j]['dow'] == 6) ? SabadoHora_M = hora: '';
-                            (data[j]['dow'] == 0) ? DomingoHora_M = data[j]['horam'] + data[j]['horasalida']: '';
+                            (data[j]['dow'] == 0) ? DomingoHora_M = data[j]['horam'] + ' - ' + data[j]['horasalida']: '';
                         } else {
                             semana = data[j]['semana'];
                             if (data[i]['f_nombremes'] == data[j]['f_nombremes']) {
