d452efb2df14cd4ece985c6ceada121d513ad775
Jbaca2022 <108754686+Jbaca2022@users.noreply.github.com>
2023-02-08 21:20:14 -0500

    modified:   vista/asistencia/inasistenciadiaria.php 	modified:   vista/css/asistencia-personal.css 	new file:   vista/css/inasistencia_print.css 	modified:   vista/css/navbar_index.css 	modified:   vista/js/script_inasistenciadiaria.js 	modified:   vista/sidebar.html

--- vista/js/script_inasistenciadiaria.js
+++ vista/js/script_inasistenciadiaria.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
                 html += '<table id="tbl-inasistencia" cellspacing="0" width="100%">';
                 html += '<thead class="cabecera">';
                 html += '    <tr>';
+                html += '        <th>N°</th>';
                 html += '        <th>Apellido y Nombres</th>';
                 html += '        <th>Aula</th>';
                 html += '        <th>Carrera</th>';
@@ -20,7 +21,8 @@ $(document).ready(function () {
                 html += '<tbody id="tbody-inasistencia">';
                 for (var i = 0; i < data.length; i++) {
                     html += '<tr class="tr-persona" id="'+ data[i].idpersona+'" data-nombre="'+ data[i].alumno +'"> '
-                    html += '<td class="text-center">' + data[i].alumno + "</td>";
+                    html += '<td class="text-center">' + (i + 1) + "</td>";
+                    html += '<td>' + data[i].alumno + "</td>";
                     html += '<td class="text-center">' + data[i].aula + "</td>";
                     html += '<td class="text-center">' + data[i].carrera + "</td>";
                     html += '<td class="text-center">' + data[i].telefono + "</td>";
@@ -49,12 +51,13 @@ $(document).ready(function () {
     $('#btnimprimir').click(function () {
         var css = '';
         css += '<link href="../../vista/css/bootstrap-print.css" rel="stylesheet" type="text/css" />';
+        css += '<link href="../../vista/css/inasistencia_print.css" rel="stylesheet" type="text/css" />';
         var objeto = '';
         objeto += '<h2><center><b>REPORTE DE INASISTENCIA</b></center></h2>'; 
         objeto += '<h3><center>'+ $('#txtfecha').val() +'</center></h3>'; //obtenemos el objeto a imprimir
         objeto += $('.divimprimir').html(); //obtenemos el objeto a imprimir
         objeto += '<script>window.print();window.close();</script>';
         var ventana = window.open('', '_blank'); //abrimos una ventana vacía nueva
-        ventana.document.write(css + objeto);
+        ventana.document.write(css + objeto);x
     });
 });
